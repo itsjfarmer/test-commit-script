0fa9517ca144d59a062766c1c1cfe12d71241d2b
Jason Farmer <jfarmer@MacBook-Pro.localdomain>
2018-06-22 16:35:03 -0400

    first

---
+++ checkinScript.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+for counter in {1..10}
+do
+   echo $counter. "\$start = " $start " and \$end = " $end
+   sed -i -e "s/JAS-[0-9]*/JAS-${end}/g" test-2/Jenkinsfile
+cd test-1
+git commit -m "update for JAS-${end}" .
+git push
+cd ..
+   if [[ $start -eq 1 ]];
+   then
+      start=0
+   fi
+   start=$(($start+1))
+   end=$(($end+1))
+   if [[ $end -ge 10 ]];
+   then
+      break
+   fi
+done
